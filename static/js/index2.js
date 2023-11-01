eed7a893abbaeb86632883713cf9aa015e103a9d
Tanntech <1320203939@bitraipur.ac.in>
2023-11-01 16:37:50 +0530

    Main files of website

---
+++ static/js/index2.js
@@ -0,0 +1,16 @@
+// const activePage = window.location.pathname;
+// const navLinks = document.querySelectorAll( '#nav-link a').
+// forEach(link => {
+// if (link.href.includes(`${activePage}`)){
+//     link.classList.add('active');
+// }
+// });
+
+
+
+
+
+// // function myFunction() {
+// //     document.getElementById("demo").style.display="block";
+// // };
+
